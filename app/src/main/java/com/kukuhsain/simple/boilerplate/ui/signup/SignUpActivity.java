9178209a2dc9d5819b0aaf54106f7d136245489d
Kukuh <kukuhsain@gmail.com>
2016-12-31 20:45:48 +0700

    Add SignUpActivity and its layout

---
+++ app/src/main/java/com/kukuhsain/simple/boilerplate/ui/signup/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.kukuhsain.simple.boilerplate.ui.signup;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kukuhsain.simple.boilerplate.R;
+
+/**
+ * Created by kukuh on 31/12/16.
+ */
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
