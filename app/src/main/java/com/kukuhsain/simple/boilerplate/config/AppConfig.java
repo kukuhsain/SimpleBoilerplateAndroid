dc48f40338a05444140fc520ab73b2751b401065
Kukuh <kukuhsain@gmail.com>
2017-03-31 20:24:54 +0700

    Add AppConfig in config package

---
+++ app/src/main/java/com/kukuhsain/simple/boilerplate/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.kukuhsain.simple.boilerplate.config;
+
+/**
+ * Created by kukuh on 31/03/17.
+ */
+
+public class AppConfig {
+
+    public static String OPEN_WEATHER_MAP_APP_ID = "cef209f6f46bb527dba385b81b808ce9";
+
+}
